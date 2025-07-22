41c194a4cc3044a667ca5021a906090f131eb8e1
Larsaikema <l.aikema@partyhuren.nl>
2025-07-22 13:29:01 +0200

    Fix Sonner import error in App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { Toaster } from "./components/ui/toaster";
-import { Sonner } from "./components/ui/sonner";
+import { Toaster as Sonner } from "./components/ui/sonner";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CustomCursor from "./components/CustomCursor";
